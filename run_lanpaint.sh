cc098e3b32d2f0e6645d4fe6dc413be312d9f969
byzenma <baizhang.ma@gmail.com>
2026-04-02 09:29:08 +0800

    install

--- run_lanpaint.sh
+++ run_lanpaint.sh
@@ -27,16 +27,16 @@
 #     --mask "https://raw.githubusercontent.com/scraed/LanPaint/master/examples/Example_9/Masked_Load_Me_in_Loader.png"
 
 # Z-Image Turbo Inpaint
-# python run_lanpaint.py --model z-image \
-#     --lp-n-steps 5 \
-#     --lp-friction 15.0 \
-#     --lp-lambda 16 \
-#     --seed 0 \
-#     --guidance-scale 1.0 \
-#     --num-steps 9 \
-#     --prompt "Latina female with thick wavy hair, white shirt, harbor boats and pastel houses behind. Breezy seaside light, warm tones, cinematic close-up." \
-#     --image "https://raw.githubusercontent.com/scraed/LanPaint/master/examples/Example_21/Original_No_Mask.png" \
-#     --mask "https://raw.githubusercontent.com/scraed/LanPaint/master/examples/Example_21/Masked_Load_Me_in_Loader.png"
+ python run_lanpaint.py --model z-image \
+     --lp-n-steps 5 \
+     --lp-friction 15.0 \
+     --lp-lambda 16 \
+     --seed 0 \
+     --guidance-scale 1.0 \
+     --num-steps 9 \
+     --prompt "Latina female with thick wavy hair, white shirt, harbor boats and pastel houses behind. Breezy seaside light, warm tones, cinematic close-up." \
+     --image "https://raw.githubusercontent.com/scraed/LanPaint/master/examples/Example_21/Original_No_Mask.png" \
+     --mask "https://raw.githubusercontent.com/scraed/LanPaint/master/examples/Example_21/Masked_Load_Me_in_Loader.png"
 
 # Z-Image Turbo Outpaint
 # python run_lanpaint.py --model z-image \
@@ -51,11 +51,11 @@
 #     --outpaint-pad "l200r200t200b200" \
 
 # Qwen Image Edit Inpaint
-python run_lanpaint.py --model qwen \
-    --prompt "change the girl's cloth to red evening gown" \
-    --image "https://raw.githubusercontent.com/scraed/LanPaint/master/examples/Example_14/Original_No_Mask.png" \
-    --mask "https://raw.githubusercontent.com/scraed/LanPaint/master/examples/Example_14/Masked_Load_Me_in_Loader.png" \
-    --seed 0 \
-    --num-steps 20 \
-    --guidance-scale 2.5 \
-    --lp-n-steps 5
+#python run_lanpaint.py --model qwen \
+#    --prompt "change the girl's cloth to red evening gown" \
+#    --image "https://raw.githubusercontent.com/scraed/LanPaint/master/examples/Example_14/Original_No_Mask.png" \
+#    --mask "https://raw.githubusercontent.com/scraed/LanPaint/master/examples/Example_14/Masked_Load_Me_in_Loader.png" \
+#    --seed 0 \
+#    --num-steps 20 \
+#    --guidance-scale 2.5 \
+#    --lp-n-steps 5
